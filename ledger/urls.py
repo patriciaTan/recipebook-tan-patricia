e0fc4451ab38964a0d2f22309e20375a79f30bfd
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-03-04 19:48:05 +0800

    Created admin panel for Recipe with RecipeIngredient as Inline Admin; modified urlpatterns

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('recipe/list', RecipeListView.as_view(), name='recipe_list'),
+    path('recipes/list', RecipeListView.as_view(), name='recipe_list'),
     path('<int:pk>/detail', RecipeDetailView.as_view(), name='recipe_detail'),
 ]
 
