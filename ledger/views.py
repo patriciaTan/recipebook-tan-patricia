226b706f6c3dce2c579cecab58723d22aed87698
Patricia Tan <157680599+patriciaTan@users.noreply.github.com>
2024-03-04 19:58:15 +0800

    Restored instructional comments for uniformity

--- ledger/views.py
+++ ledger/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
 from .models import Recipe
-
 # Create your views here.
+
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipe_list.html'
